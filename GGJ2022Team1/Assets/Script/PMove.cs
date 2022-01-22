222b0b3c243abc8683ddc6e4d61efa538704e992
Sir-Zompi <79336944+Sir-Zompi@users.noreply.github.com>
2022-01-22 03:13:15 +0100

    Movimento migliorato (Roberto)

---
+++ GGJ2022Team1/Assets/Script/PMove.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PMove : MonoBehaviour
+{
+    public enum PrioritisedInput
+    {
+        None,
+        XAxis,
+        YAxis
+    }
+
+    public PrioritisedInput InputConPriorita = PrioritisedInput.None;
+
+    //------------------------------------------------------------------------------------------
+    //--------------VARIABILI-------------------------------------------------------------------
+
+    [SerializeField]
+    public float velMovimento = 5f;
+
+    //componenti
+    Rigidbody2D rb2D;
+
+    //variabili locali
+    public Vector2 DirMovimento;
+
+    //------------------------------------------------------------------------------------------
+    //---------------METODI---------------------------------------------------------------------
+
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //Asseggno a movement l'input nell'asse X e Y
+        DirMovimento.x = Input.GetAxisRaw("Horizontal");
+        DirMovimento.y = Input.GetAxisRaw("Vertical");
+
+        if (DirMovimento.y == 0 && DirMovimento.x == 0)
+        {
+            // Reimposta il valore se non è stato rilevato alcun input 
+            InputConPriorita = PrioritisedInput.None;
+        }
+        else if (DirMovimento.y != 0 && DirMovimento.x == 0)
+        {
+            // Ricevi asse X input, ma nessun input dell'asse Y. Priorità Asse y
+            InputConPriorita = PrioritisedInput.YAxis;
+        }
+        else if (DirMovimento.y == 0 && DirMovimento.x != 0)
+        {
+            // ricevi asse Y input, ma nessun input dell'asse X. Priorità Asse X
+            InputConPriorita = PrioritisedInput.XAxis;
+        }
+        else if (DirMovimento.y != 0 && DirMovimento.x != 0 && InputConPriorita == PrioritisedInput.None)
+        {
+            // Cosa succede se entrambi gli assi ricevono un input nello stesso frame?
+            // Da la priorità all'asse X
+            InputConPriorita = PrioritisedInput.XAxis;
+        }
+
+        if (InputConPriorita == PrioritisedInput.XAxis)
+        {
+            DirMovimento.y = 0;
+        }
+        else if (InputConPriorita == PrioritisedInput.YAxis)
+        {
+            DirMovimento.x = 0;
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        rb2D.MovePosition(rb2D.position + DirMovimento * velMovimento * Time.fixedDeltaTime);
+    }
+}
